42758a58bb307edf25e93dffc2943743cfd3b239
MALACRIAY <165767718+MALACRIAY@users.noreply.github.com>
2024-06-11 13:23:39 +0200

    1ra prova
    pensar 2na prova,etc acabar posar sprites reals etc

--- scenes/Camera barco.gd
+++ scenes/Camera barco.gd
@@ -1,6 +1,6 @@
 extends Camera2D
-
-
+@onready var barco = $"../CharacterBody2D"
+var entered = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +8,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y = $"../CharacterBody2D".position.y
+	position.y = barco.position.y
+	if entered:
+		barco.global_position.y = move_toward(barco.global_position.y,505,barco.VELOCITY)
+		if barco.rotation <= 90:
+			barco.rotation += delta
+func _on_area_2d_body_entered(body):
+	if body == barco:
+		barco.entered = true
+		entered = true
+	
--- scenes/barco.gd
+++ scenes/barco.gd
@@ -3,14 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const VELOCITY = 3
-
+var entered = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
 
 func _physics_process(delta):
 
 	#rotation
-	var mouse = get_global_mouse_position()
-	global_position.x = move_toward(global_position.x,mouse.x,VELOCITY)
-	velocity.y = SPEED
+	if not entered:
+		var mouse = get_global_mouse_position()
+		global_position.x = move_toward(global_position.x,mouse.x,VELOCITY)
+		velocity.y = SPEED
+		
 	move_and_slide()
